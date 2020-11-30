af9fc5879666b48986812d65bb63a03c0fadd46f
PhatTanNguyen <63003240+PhatTanNguyen@users.noreply.github.com>
2020-11-30 20:15:20 +0700

    Chỉnh sửa tiếp giao diện

---
+++ mvcDongHo/Areas/Admin/Controllers/QuanLiHoaDonBanController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using mvcDongHo.Areas.Admin.ViewModels;
+
+namespace mvcDongHo.Areas.Admin.Controllers
+{
+    [Area ("Admin")]
+    public class QuanLiHoaDonBanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HoaDonBan()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvcDongHo/Areas/Admin/Controllers/QuanLiHoaDonNhapController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcDongHo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class QuanLiHoaDonNhapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult HoaDonNhap()
+        {
+            return View();
+        }
+    }
+}
