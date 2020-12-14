dec410c813b4134c4ef5a432ef5bc0154c61cae0
Quang Huy <quanghuy01112000@gmail.com>
2020-12-15 03:10:47 +0700

    Demo

--- mvcDongHo/Controllers/HomeController.cs
+++ mvcDongHo/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using Application.Interfaces;
 using Application.DTOs;
 using mvcDongHo.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
 namespace mvcDongHo.Controllers
 {
     public class HomeController : Controller
@@ -28,10 +31,6 @@ namespace mvcDongHo.Controllers
             return View();
         }
         public ActionResult About()
-        {
-            return View();
-        }
-        public ActionResult Cart()
         {
             return View();
         }
@@ -76,5 +75,124 @@ namespace mvcDongHo.Controllers
         //     var content = list.Where(i=>i.TenSanPham.Contains(textSearch)).ToList();
         //     return Json(content);
         // }
+
+
+        //Phần giỏ hàng
+        //Thêm sản phẩm vào giỏ hàng
+        public IActionResult addCart(String id)
+        {
+            var cart = HttpContext.Session.GetString("cart");//get key cart 
+            if (cart == null) //sau đó kiểm tra có tồn tại không
+            {
+                //Nếu bằng null thì tạo list chứa sản phẩm đầu tiên 
+                var item = _sanPhamServices.Xemsp(id);
+                List<CartItem> listCart = new List<CartItem>()
+               {
+                   new CartItem
+                   {
+                       sanPhamDTO = item,
+                       quantityItem = 1
+                   }
+               };
+                //Sau đó tạo session với key "cart"
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(listCart));
+
+            }
+            else
+            {
+                // nếu khác null thì dùng JsonConvert.DeserializeObject chuyển đổi json về dạng list 
+                List<CartItem> dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                bool check = false; //biến check để kiểm tra sản phẩm vừa thêm đã có trong giỏ hàng hay chưa.
+                //sau đó kiểm tra ID sản phẩm với id nếu có tồn tại trong giỏ hàng sẵn rồi thì tăng số lượng
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].sanPhamDTO.IDSanPham == id)
+                    {
+                        dataCart[i].quantityItem++;
+                        check = true; //chuyển biến check để có tồn tại
+                    }
+                }
+                //Nếu chưa tồn tại thì thêm mới vào và số lượng là 1
+                if (!check)
+                {
+                    dataCart.Add(new CartItem
+                    {
+                        sanPhamDTO = _sanPhamServices.Xemsp(id),
+                        quantityItem = 1
+                    });
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                // var cart2 = HttpContext.Session.GetString("cart");//get key cart
+                //  return Json(cart2);
+            }
+            //Quay lại trang index
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        //Dùng để hiển thị sản phẩm trong giỏ hàng
+        public IActionResult Cart()
+        {
+            var cart = HttpContext.Session.GetString("cart");//get key cart
+            if (cart != null)
+            {
+                List<CartItem> dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                if (dataCart.Count > 0)
+                {
+                    ViewBag.carts = dataCart; //gán list sản phẩm trong giỏ hàng cho "cart"
+                    return View();
+                }
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Hàm update sản phẩm 
+        [HttpPost]
+        public IActionResult updateCart(String id, int quantity)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<CartItem> dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                if (quantity > 0)
+                {
+                    for (int i = 0; i < dataCart.Count; i++)
+                    {
+                        if (dataCart[i].sanPhamDTO.IDSanPham == id)
+                        {
+                            dataCart[i].quantityItem = quantity;
+                        }
+                    }
+                    HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                }
+                var cart2 = HttpContext.Session.GetString("cart");
+                return Ok(quantity);
+            }
+            return BadRequest();
+
+        }
+
+        //Kiểm tra sản phẩm có id là ... có trong giỏ hàng hay không . nếu có thì cho nó bay màu
+        public IActionResult deleteCart(String id)
+        {
+            var cart = HttpContext.Session.GetString("cart");
+            if (cart != null)
+            {
+                List<CartItem> dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+
+                for (int i = 0; i < dataCart.Count; i++)
+                {
+                    if (dataCart[i].sanPhamDTO.IDSanPham == id)
+                    {
+                        dataCart.RemoveAt(i);
+                    }
+                }
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(dataCart));
+                return RedirectToAction(nameof(Cart));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+
     }
 }
--- mvcDongHo/wwwroot/js/MyWeb.js
+++ mvcDongHo/wwwroot/js/MyWeb.js
@@ -1,5 +1,13 @@
  //Hàm xóa thương hiệu
  //viết jquery, do xài cái model để xác nhận có muốn xóa hay không thì mình không lấy mã trực tiếp như cái sửa được nên phải viết thêm cái này  
+$("button#xoathuonghieu").click(function ()
+//  tạo sự kiên click cho cái button ở trên theo cú pháp của jquery: "tenthe#mã thẻ" 
+{
+    var id = $(this).data("id");//gán IDThuongHieu đã được lưu vào data-id ở trên để gán vào biến id
+    var obj = '<a class="btn btn-success" href="https://localhost:5001/Admin/QuanLiThuongHieu/XoaThuongHieuData/' + id + '">Có</a>'
+        + '<button type="button" class="btn btn-secondary" data-dismiss="modal">Không</button>';//tạo biến obj để lưu lại cái html mình cần xuất ra, cái này tui không quá rõ nhưng mà cứ làm theo thôi, hok khó
+    $("div#footer-del").html(obj);//gọi cái thẻ div có mã là footer-del để chèn phần html zo
+});
 $('button#xemchitietban').click(function ()
 {
     var id = $(this).data("id");
@@ -44,4 +52,5 @@ $('button#xemchitietnhap').click(function ()
         }
     });   
 });
+
     
