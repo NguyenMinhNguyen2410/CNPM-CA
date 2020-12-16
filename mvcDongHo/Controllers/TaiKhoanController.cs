3373e13ed445e313d384cf9c73c71112a09472ac
Quang Huy <quanghuy01112000@gmail.com>
2020-12-16 15:28:30 +0700

    Sửa giao diện giỏ hàng

--- mvcDongHo/Controllers/TaiKhoanController.cs
+++ mvcDongHo/Controllers/TaiKhoanController.cs
@@ -6,20 +6,36 @@ using System.Threading.Tasks;
 using Application.Interfaces;
 using Application.DTOs;
 using mvcDongHo.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
 namespace mvcDongHo.Controllers
 {
     public class TaiKhoanController : Controller
     {
+        private readonly ISanPhamServices _sanPhamServices;//khai báo services
         private readonly ITaiKhoanKHServices _taiKhoanKHServices;//khai báo services
-        public TaiKhoanController(ITaiKhoanKHServices taiKhoanKHServices) //contructor
+        public TaiKhoanController(ITaiKhoanKHServices taiKhoanKHServices, ISanPhamServices sanPhamServices) //contructor
         {
             _taiKhoanKHServices = taiKhoanKHServices;
+            _sanPhamServices = sanPhamServices;
         }
         public IActionResult Checkout()
         {
             var list = _taiKhoanKHServices.getAll();//hàm lấy tất cả các đối tượng ở dưới database để show thông tin sản phẩm lên
             ViewBag.List = list;//lưu danh sách vừa lấy được vào viewbag đê show ra bên index
-            return View();
+            //Giỏ hàng
+            var cart = HttpContext.Session.GetString("cart");//get key cart
+            if (cart != null)
+            {
+                List<CartItem> dataCart = JsonConvert.DeserializeObject<List<CartItem>>(cart);
+                if (dataCart.Count > 0)
+                {
+                    ViewBag.carts = dataCart; //gán list sản phẩm trong giỏ hàng cho "cart"
+                    return View();
+                }
+            }
+            return RedirectToAction(nameof(Index));
         }
         public ActionResult ThemTKData(TaiKhoanView taiKhoanView)
         {
@@ -41,5 +57,9 @@ namespace mvcDongHo.Controllers
         {
             return View();
         }
+
+        //Dùng để hiển thị sản phẩm trong giỏ hàng
+        
+
     }
 }
